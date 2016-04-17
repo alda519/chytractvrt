299fd5a5df378c0dc7468b3084c85dd5ad43e7bd
Alda <galda519@gmail.com>
2016-04-17 22:52:48 +0200

    pridani ostatnich stranek

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,5 @@
 import os
-#import psycopg2
+import psycopg2
 from datetime import datetime
 from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort, send_from_directory
@@ -7,10 +7,55 @@ from flask import Flask, request, flash, url_for, redirect, \
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
 
+"""
+g = None
+
+@app.before_request
+def before_request():
+    #g.db = psycopg2.connect(flask.config.DB_STR)
+    print(flask.config.DB_STR)
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, db, None)
+    if db is not None:
+        db.close()
+"""
+
 @app.route('/')
+@app.route('/mapa')
 def index():
     return render_template('map.html')
-#    return render_template('index.html')
+
+@app.route('/okoli')
+def okoli():
+    return render_template('okoli.html')
+
+@app.route('/spolecnost')
+def spolecnost():
+    return render_template('spolecnost.html')
+
+@app.route('/domy')
+def domy():
+    return render_template('domy.html')
+
+@app.route('/mimoradne')
+def mimoradne():
+    return render_template('mimoradne.html')
+
+
+@app.route('/points')
+@app.route('/points/<id>')
+def points(id=None):
+    if request.method == 'POST':
+        # TODO update
+        return render_template('points.html')
+    else:
+        # get 1 nebo seznam vsech
+        if id in not None:
+            return render_template('point.html')
+        else:
+            return render_template('points.html')
 
 
 @app.route('/<path:resource>')
@@ -19,12 +64,6 @@ def serveStaticResource(resource):
 
 @app.route("/test")
 def test():
-    DB_HOST=os.environ['OPENSHIFT_POSTGRESQL_DB_HOST']
-    DB_PORT=os.environ['OPENSHIFT_POSTGRESQL_DB_PORT']
-    DB_USER=os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME']
-    DB_PSWD=os.environ['OPENSHIFT_POSTGRESQL_DB_PASSWORD']
-    DB_STR="host='{}' port='{}' user='{}' password='{}'".format(
-        DB_HOST, DB_PORT, DB_USER, DB_PSWD)
     conn = psycopg2.connect(DB_STR)
     cursor = conn.cursor()
     cursor.execute('select * from categories;')
@@ -34,9 +73,9 @@ def test():
 @app.route("/getpoints/<category>")
 def getpoints(category):
     if int(category) == 1:
-        return '[{"name": "uno", "lat": 49.1791, "lng": 16.5554}, {"name": "uno", "lat": 49.1781, "lng": 16.5544}, {"name": "uno", "lat": 49.1771, "lng": 16.5534}]'
+        return '[{"name": "uno", "lat": 49.1791, "lng": 16.5554, "cat": 1}, {"name": "uno", "lat": 49.1781, "lng": 16.5544, "cat": 1}, {"name": "uno", "lat": 49.1771, "lng": 16.5534, "cat": 1}]'
     else:
-        return '[{"name": "uno", "lat": 49.1722, "lng": 16.5534}, {"name": "uno", "lat": 49.1711, "lng": 16.5534}, {"name": "uno", "lat": 49.1701, "lng": 16.5534}]'
+        return '[{"name": "uno", "lat": 49.1722, "lng": 16.5534, "cat": 2}, {"name": "uno", "lat": 49.1711, "lng": 16.5534, "cat": 2}, {"name": "uno", "lat": 49.1701, "lng": 16.5534, "cat": 2}]'
 
 
 if __name__ == '__main__':
