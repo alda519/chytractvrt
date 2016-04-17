c6c0bdeec2f2e3c31029041a9d0fa480e24b3b24
alda <galda519@gmail.com>
2016-04-17 09:48:55 +0200

    pg test

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 from datetime import datetime
 from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort, send_from_directory
@@ -18,7 +19,17 @@ def serveStaticResource(resource):
 
 @app.route("/test")
 def test():
-    return "<strong>It's Alive!</strong>"
+    DB_HOST=os.environ['OPENSHIFT_POSTGRESQL_DB_HOST']
+    DB_PORT=os.environ['OPENSHIFT_POSTGRESQL_DB_PORT']
+    DB_USER=os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME']
+    DB_PSWD=os.environ['OPENSHIFT_POSTGRESQL_DB_PASSWORD']
+    DB_STR="host='{}' port='{}' user='{}' password='{}'".format(
+        DB_HOST, DB_PORT, DB_USER, DB_PSWD)
+    conn = psycopg2.connect(DB_STR)
+    cursor = conn.cursor()
+    cursor.execute('select * from categories;')
+    records = cursor.fetchall()
+    return "<strong>It's Alive!</strong> " + ( ';'.join([x[1] for x in records])")
 
 if __name__ == '__main__':
     app.run()
